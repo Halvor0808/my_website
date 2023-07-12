8dca0fb1433966ae65a53da1f4a303a8355f9b03
Halvor Brunt <halvor@brunt.no>
2023-07-12 13:58:15 +0200

    Removed all tailwind to begin with

--- website/src/app/components/accounts.js
+++ website/src/app/components/accounts.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 const Accounts = () => {
     return (
-        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="">
+            <div className="">
                 <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                className=""
                 href="https://github.com/Halvor0808"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -22,9 +22,9 @@ const Accounts = () => {
                 />
                 </a>
             </div>
-            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <div className="">
                 <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                className=""
                 href="https://www.linkedin.com/in/halvor-brunt/"
                 target="_blank"
                 rel="noopener noreferrer"
