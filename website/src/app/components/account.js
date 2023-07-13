144340d63ce1a28ed5ada3dc26f49d83e4be66bd
Halvor Brunt <halvor@brunt.no>
2023-07-13 13:30:58 +0200

    Update account

--- website/src/app/components/account.js
+++ website/src/app/components/account.js
@@ -2,20 +2,18 @@ import Image from 'next/image';
 
 
 const Account = (props) => {
-    const dark_invert = props.dark_invert;
-    const newClassName = dark_invert + ''; // Can add more to class here
     return (
         <a
-            className="p-3 pt-1 pb-2 rounded-xl bg-neutral-800 m-2"
+            className="p-3 pt-1 pb-2 rounded-xl m-2 bg-neutral-800 hover:bg-neutral-700 text-gray-300 hover:text-white"
             href={props.href}
             target="_blank"
             rel="noopener noreferrer"
         >
-            <h2 className='text-center p-0 text-white'>{props.title}</h2>
+            <h2 className='text-center p-0'>{props.title}</h2>
                 <Image
                     src={props.src}
                     alt={props.alt}
-                    className={dark_invert}
+                    className={props.dark_invert}
                     width={96}
                     height={96}
                     priority
