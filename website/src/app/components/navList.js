8866c185ba9b9a578085eaf8b017c19af760ac35
Halvor Brunt <halvor@brunt.no>
2023-08-13 14:41:14 +0200

    Dysfunctional navbar-mem for ative tab

--- website/src/app/components/navList.js
+++ website/src/app/components/navList.js
@@ -1,38 +1,37 @@
-
-import React from "react";
+'use client'
+import { React, useState } from "react";
+import Link from "next/link";
 
 const NavList = () => {
+
+  const [activeTab, setActiveTab] = useState("home")
+  const handleTabSelection = (tab) => {
+    setActiveTab(tab);
+  }
+  
   return (
     <nav className="bg-neutral-800 border-b-neutral-400/50 border-b-2 border-solid mb-6">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex space-x-4">
-              {/* Current: "bg-neutral-900/50 text-white rounded-md px-3 py-2 text-sm font-medium 
-                border-neutral-900 border-2 border-solid", 
-              Default: "text-gray-300 hover:bg-neutral-700/50 hover:text-white rounded-md px-3 py-2 
-                text-sm font-medium hover:border-neutral-900 border-2 hover:border-solid"
-                style={{"border-color": "transparent" }}  */}
-              <a
-                href="/"
-                className="bg-neutral-900/50 text-white rounded-md px-3 py-2 text-sm font-medium border-neutral-900 border-2 border-solid"
-                aria-current="page"
-              >
+              <Link href="/"
+                className={activeTab === "home" ? "text-red-600" : ""}
+                onClick={() => handleTabSelection("home")}>
                 Home
-              </a>
-              <a
-                href="/projects"
-                className="text-gray-300 hover:bg-neutral-700/50 hover:text-white rounded-md px-3 py-2 text-sm font-medium hover:border-neutral-900 border-2 hover:border-solid"
-                style={{"borderColor": "transparent" }} 
-              >
-                Projects
-              </a>
+              </Link>
+
+              <Link href="/projects"
+                className={activeTab === "projects" ? "text-red-600" : ""}
+                onClick={() => handleTabSelection("projects")}>
+                About
+              </Link>
             </div>
-          </div>        
+          </div>
         </div>
       </div>
     </nav>
-  
   );
 }
+
 export default NavList;
